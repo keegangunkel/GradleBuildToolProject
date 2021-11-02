f1a0827d9081dae1bab5074c48be0aedb7ab3e6a
Gunkel <gunkeec@dunwoody.edu>
2021-11-02 18:08:21 -0500

    Finished sphere tests and added to main file. Cleaned up the code a bit and fixed the quitting bug.

--- Geometry.java
+++ Geometry.java
@@ -14,13 +14,17 @@ public class Geometry {
        Scanner reader = new Scanner(System.in);
        //While loop to create the selection menu
        while(valid){
-        System.out.println("Welcome to my geometry program!\nSelect from the following options:\n1. Cylinder\n2. Sphere\n3.) Cone\n0.) Quit");
+        System.out.println("Welcome to my geometry program!\nSelect from the following options:\n1.) Cylinder\n2.) Sphere\n3.) Cone\n0.) Quit");
         
         int answer = reader.nextInt();
 
         if(answer == 1){
             //prompts cylinder program
             cylinderPrompt();
+        }
+        else if(answer == 2){
+            //prompts sphere program
+            spherePrompt();
         }
         else if(answer == 3){
             //prompts cone program
@@ -30,6 +34,7 @@ public class Geometry {
             //Closes program
             System.out.println("Terminating program...");
             reader.close();
+            valid = false;
         }
        }
       }
@@ -89,4 +94,29 @@ public class Geometry {
       radiusReader.close();
       heightReader.close();
       }
+            //Method to prompt the sphere program
+            private static void spherePrompt()
+            {
+        
+            System.out.println("------------------------------------------------------------");
+            System.out.println("PYTHON PROGRAM TO FIND VOLUME AND SURFACE AREA OF A SPHERE");
+            System.out.println("------------------------------------------------------------");
+        
+            Scanner radiusReader = new Scanner(System.in);
+            System.out.println("Please enter the radius:");
+            int radiusInput = radiusReader.nextInt();
+            //Instantiating a local Cylinder object
+            Sphere sphere = new Sphere();
+            //invokes volume and surface area method in cylinder class to calculate 
+            double volume = sphere.volume(radiusInput);
+            double surfaceArea = sphere.surfaceArea(radiusInput);
+            //prints out volume and surface area
+            System.out.println("\nThe volume of the SPhere: " + volume); 
+            System.out.println("\nThe surface area of the Sphere: " + surfaceArea + "\n");
+            //invokes menu again
+            userMenu();
+            //Closes scanners
+            radiusReader.close();
+            
+            }
 }
