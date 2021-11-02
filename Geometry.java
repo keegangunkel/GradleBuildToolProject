12bd29b45796fa756719c65b28a530e7b81cfbb9
Gunkel <gunkeec@dunwoody.edu>
2021-11-02 17:56:31 -0500

    Created cone class and test and added it to main geometry program. Also started the sphere.

--- Geometry.java
+++ Geometry.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Geometry {
+
+    //Main method to initiate user menu
     public static void main(String args[]){
         userMenu();
     }
+
     public static void userMenu(){
        //Creating a boolean to continue/break the while loop
        boolean valid = true;
@@ -16,14 +19,21 @@ public class Geometry {
         int answer = reader.nextInt();
 
         if(answer == 1){
+            //prompts cylinder program
             cylinderPrompt();
         }
-        if(answer == 0){
+        else if(answer == 3){
+            //prompts cone program
+            conePrompt();
+        }
+        else if(answer == 0){
+            //Closes program
             System.out.println("Terminating program...");
             reader.close();
         }
        }
       }
+      //Method to prompt the cylinder program
       private static void cylinderPrompt()
       {
   
@@ -45,6 +55,34 @@ public class Geometry {
       //prints out volume and surface area
       System.out.println("\nThe volume of the Cylinder: " + volume); 
       System.out.println("\nThe surface area of the Cylinder: " + surfaceArea + "\n");
+       //invokes menu again
+      userMenu();
+      //Closes scanners
+      radiusReader.close();
+      heightReader.close();
+      }
+      //Method to prompt the cone program
+      private static void conePrompt()
+      {
+  
+      System.out.println("------------------------------------------------------------");
+      System.out.println("PYTHON PROGRAM TO FIND VOLUME AND SURFACE AREA OF A CONE");
+      System.out.println("------------------------------------------------------------");
+  
+      Scanner radiusReader = new Scanner(System.in);
+      System.out.println("Please enter the radius:");
+      int radiusInput = radiusReader.nextInt();
+      System.out.println("Please enter the height:");
+      Scanner heightReader = new Scanner(System.in);
+      int heightInput = heightReader.nextInt();
+      //Instantiating a local Cylinder object
+      Cone cone = new Cone();
+      //invokes volume and surface area method in cylinder class to calculate 
+      double volume = cone.volume(radiusInput, heightInput);
+      double surfaceArea = cone.surfaceArea(radiusInput, heightInput);
+      //prints out volume and surface area
+      System.out.println("\nThe volume of the Cone: " + volume); 
+      System.out.println("\nThe surface area of the Cone: " + surfaceArea + "\n");
        //invokes menu again
       userMenu();
       //Closes scanners
